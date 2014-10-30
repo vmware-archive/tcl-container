1eaeb1d2b00097beb68579df6637c62c0ff3c9fa
Oliver Kurth <okurth@vmware.com>
2014-10-29 17:01:44 -0700

    put tarballs in directory, make sure libdnet builds 32bit

--- README.md
+++ README.md
@@ -13,5 +13,8 @@ sudo docker build .
 This can then be used to build open-vm-tools and libdnet, using the file build.sh:
 
 sudo docker tag 53a89975885d tinycorelinux:ovt-devel
-sudo docker run -t -i tinycorelinux:ovt-devel /root/build.sh
 
+The build script will put the tarballs into the directory /tarballs, so you
+need to create a local dir and map it to the container with the -v option:
+
+sudo docker run  -v ${PWD}/tarballs:/tarballs -t -i ovt-devel /root/build.sh
--- build.sh
+++ build.sh
@@ -8,6 +8,11 @@ LIBDNET_URL=http://sourceforge.net/projects/libdnet/files/libdnet/${LIBDNET}/${L
 OVT=open-vm-tools-9.4.0-1280544
 OVT_URL=http://sourceforge.net/projects/open-vm-tools/files/open-vm-tools/stable-9.4.x/${OVT}.tar.gz
 
+if [ ! -d /tarballs ]; then
+	echo "/tarballs directory not found"
+	exit 1
+fi
+
 cd /root
 wget ${LIBDNET_URL}
 wget ${OVT_URL}
@@ -17,8 +22,9 @@ mkdir /tmp/stage
 cd /root
 tar zxf ${LIBDNET}.tar.gz 
 cd ${LIBDNET}
-./configure && make && make install && make DESTDIR=/tmp/stage/ install
+./configure --host=i486-pc-linux-gnu && make && make install && make DESTDIR=/tmp/stage/libdnet install
 
+(cd /tmp/stage/libdnet && tar zcf /tarballs/libdnet.tgz .)
 
 cd /root
 tar zxf ${OVT}.tar.gz 
@@ -32,5 +38,7 @@ done
 # detected correctly in a container running in a 64bit host
 ./configure --without-kernel-modules --without-pam --without-x --without-icu --host=i486-pc-linux-gnu && \
   make LIBS="-ltirpc" CFLAGS='-Wno-deprecated-declarations' && \
-  make DESTDIR=/tmp/stage/ install
+  make DESTDIR=/tmp/stage/open-vm-tools install
+
+(cd /tmp/stage/open-vm-tools && tar zcf /tarballs/open-vm-tools.tgz .)
 
