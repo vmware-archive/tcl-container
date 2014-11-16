97d07500c58101a6b12f983679a30bfe9c1b80d2
Oliver Kurth <okurth@gmail.com>
2014-11-15 22:31:43 -0800

    upgrade to open-vm-tools 9.4.6 (using autoreconf)

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 LIBDNET=libdnet-1.11
 LIBDNET_URL=http://sourceforge.net/projects/libdnet/files/libdnet/${LIBDNET}/${LIBDNET}.tar.gz
 
-OVT=open-vm-tools-9.4.0-1280544
+OVT=open-vm-tools-9.4.6-1770165
 OVT_URL=http://sourceforge.net/projects/open-vm-tools/files/open-vm-tools/stable-9.4.x/${OVT}.tar.gz
 
 if [ ! -d /tarballs ]; then
@@ -36,6 +36,7 @@ done
 
 # we need to set --host because boot2docker is 32 bit, and this will not be
 # detected correctly in a container running in a 64bit host
+autoreconf -i && \
 ./configure --without-kernel-modules --without-pam --without-x --without-icu --host=i486-pc-linux-gnu && \
   make LIBS="-ltirpc" CFLAGS='-Wno-deprecated-declarations' && \
   make DESTDIR=/tmp/stage/open-vm-tools install
